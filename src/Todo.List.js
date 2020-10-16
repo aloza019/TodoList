b938f15f22710dcb1bf1bb9643859f672f14681e
ANDRES LOZANO <andreslozano@ANDRESs-MacBook-Pro-2.local>
2020-10-15 21:54:49 -0400

    first commit

---
+++ src/Todo.List.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import TodoItem from '../src/TodoItem';
+//import './component.css';
+
+class TodoList extends React.Component {
+
+    render() {
+    return (  
+        <div>
+        {this.props.items.map(item => (
+        <TodoItem id={item.id} status={item.status} task={item.task} deleteItem={this.props.deleteItem} markItemComplete={this.props.markItemComplete} />
+        ))} 
+    </div>
+        
+    );
+  } 
+}
+
+export default TodoList;
